afc8a305677a39fb1c3ae34257417825b0476c62
SalehinShohan <ps.shohan1@gmail.com>
2023-06-07 23:38:42 +0600

    load data from database mongodb in class data and instructor data

--- index.js
+++ index.js
@@ -27,6 +27,34 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const classCollection = client.db("CreativeConnotationsDB").collection("classes");
+    const instructorsCollection = client.db("CreativeConnotationsDB").collection("instructors");
+
+    app.get('/class', async(req, res) => {
+        const result = await classCollection.find().toArray();
+        res.send(result);
+    })
+
+    app.get('/instructor', async(req, res) => {
+        const result = await instructorsCollection.find().toArray();
+        res.send(result);
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
