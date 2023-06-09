76ce67358b693d8a272660fbb56a1a5da2427fa6
SalehinShohan <ps.shohan1@gmail.com>
2023-06-09 10:20:43 +0600

    add admin check api

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
-require('dotenv').config()
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
+  }
+
+  const token = authorization.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_JWT, (err, decoded) => {
+    if (err) {
+      return res
+        .status(403)
+        .send({ error: true, message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
 
-
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jxsnyx4.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -21,7 +40,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -29,115 +48,136 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const classCollection = client.db("CreativeConnotationsDB").collection("classes");
-    const instructorsCollection = client.db("CreativeConnotationsDB").collection("instructors");
-    const reviewCollection = client.db("CreativeConnotationsDB").collection("reviews");
-    const cartCollection = client.db("CreativeConnotationsDB").collection("carts");
-    const userCollection = client.db("CreativeConnotationsDB").collection("users");
+    const classCollection = client
+      .db("CreativeConnotationsDB")
+      .collection("classes");
+    const instructorsCollection = client
+      .db("CreativeConnotationsDB")
+      .collection("instructors");
+    const reviewCollection = client
+      .db("CreativeConnotationsDB")
+      .collection("reviews");
+    const cartCollection = client
+      .db("CreativeConnotationsDB")
+      .collection("carts");
+    const userCollection = client
+      .db("CreativeConnotationsDB")
+      .collection("users");
 
-    app.post('/jwt', (req, res) => {
-
+    app.post("/jwt", (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, env.process.ACCESS_TOKEN_JWT, {expiresIN: '1h'})
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_JWT, {
+        expiresIn: "1h",
+      });
 
-      res.send({token})
-    })
+      res.send({ token });
+    });
 
-    app.get('/users', async(req, res) => {
+    app.get("/users", async (req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result);
-    })
+    });
 
-    app.post('/users', async(req, res) => {
+    app.post("/users", async (req, res) => {
       const user = req.body;
-      const query = {email: user.email}
+      const query = { email: user.email };
       const existingUser = await userCollection.findOne(query);
-      if(existingUser){
-        return res.send({message: 'user already existing'})
+      if (existingUser) {
+        return res.send({ message: "user already existing" });
       }
       const result = await userCollection.insertOne(user);
       res.send(result);
+    });
 
-    })
+    app.get('/users/admin/:email', verifyJWT ,async(req, res) => {
+      const email = req.params.email;
 
-    app.patch('/users/admin/:id', async(req, res) => {
-      const id  = req.params.id;
-      const filter = {_id: new ObjectId(id)};
-
-      const updateDoc = {
-        $set: {
-          role: 'admin'
-        }
+      if(req.decoded.email !== email){
+        res.send({admin: false});
       }
-      const result = await userCollection.updateOne(filter, updateDoc);
+
+      const query = {email: email};
+      const user = await userCollection.findOne(query);
+      const result = {admin: user?.role == 'admin'}
       res.send(result);
     })
 
-
-    app.patch('/users/instructor/:id', async(req, res) => {
-      const id  = req.params.id;
-      const filter = {_id: new ObjectId(id)};
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
 
       const updateDoc = {
         $set: {
-          role: 'instructor'
-        }
-      }
+          role: "admin",
+        },
+      };
       const result = await userCollection.updateOne(filter, updateDoc);
       res.send(result);
-    })
+    });
 
+    app.patch("/users/instructor/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
 
-    app.get('/class', async(req, res) => {
-        const result = await classCollection.find().toArray();
-        res.send(result);
-    })
+      const updateDoc = {
+        $set: {
+          role: "instructor",
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
 
-    app.get('/instructor', async(req, res) => {
-        const result = await instructorsCollection.find().toArray();
-        res.send(result);
-    })
-    app.get('/review', async(req, res) => {
-        const result = await reviewCollection.find().toArray();
-        res.send(result);
-    })
+    app.get("/class", async (req, res) => {
+      const result = await classCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/instructor", async (req, res) => {
+      const result = await instructorsCollection.find().toArray();
+      res.send(result);
+    });
+    app.get("/review", async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+      res.send(result);
+    });
 
     //carts collections
-    app.get('/carts', async(req, res) => {
+    app.get("/carts", verifyJWT, async (req, res) => {
       const email = req.query.email;
-      console.log(email)
-      if(!email){
+      console.log(email);
+      if (!email) {
         res.send([]);
       }
-      const query = {email: email};
-      const result = await cartCollection.find(query).toArray()
-      res.send(result);
-    })
 
-    app.post('/carts', async(req, res) => {
+      const decodedEmail = req.decoded.email;
+      if(email !== decodedEmail){
+        return res.status(403).send({ error: true, message: "forbidden access" })
+      }
+
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/carts", async (req, res) => {
       const item = req.body;
       const result = await cartCollection.insertOne(item);
       res.send(result);
-    })
+    });
 
-    app.delete('/carts/:id', async(req, res) => {
+    app.delete("/carts/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await cartCollection.deleteOne(query);
       res.send(result);
-    })
-
-
-
-
-
-
-
-
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -145,16 +185,12 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Creative Connotations is running");
+});
 
-
-
-
-app.get('/', (req, res) => {
-    res.send('Creative Connotations is running')
-})
-
-app.listen(port, ()=> {
-    console.log(`Creative Connotations is run: ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Creative Connotations is run: ${port}`);
+});
 
 //CreativeConnotationsDB
