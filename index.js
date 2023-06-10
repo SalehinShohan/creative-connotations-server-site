eb2a770d1f8c28db5f5d445cfe49b2b3e855edb3
SalehinShohan <ps.shohan1@gmail.com>
2023-06-10 08:27:57 +0600

    add payment api

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const stripe = require("stripe")(process.env.PAYMENT_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
 //middleware
@@ -53,6 +54,7 @@ async function run() {
     const reviewCollection = client.db("CreativeConnotationsDB").collection("reviews");
     const cartCollection = client.db("CreativeConnotationsDB").collection("carts");
     const userCollection = client.db("CreativeConnotationsDB").collection("users");
+    const paymentCollection = client.db("CreativeConnotationsDB").collection("payments");
 
     app.post("/jwt", (req, res) => {
       const user = req.body;
@@ -181,6 +183,24 @@ async function run() {
       const result = await cartCollection.deleteOne(query);
       res.send(result);
     });
+
+
+    //payment api
+
+    app.post("/create-payment-intent", verifyJWT, async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    });
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
